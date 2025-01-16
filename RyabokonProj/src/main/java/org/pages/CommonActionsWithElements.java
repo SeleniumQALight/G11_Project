940561c1347173e98c3dfcb2921bf92489acb2d6
NRyabokon <nryabokon30@gmail.com>
2025-01-16 20:55:28 +0100

    First Test with page object

---
+++ RyabokonProj/src/main/java/org/pages/CommonActionsWithElements.java
@@ -0,0 +1,18 @@
+package org.pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class CommonActionsWithElements {
+    protected WebDriver webDriver;
+
+    public  CommonActionsWithElements(WebDriver webDriver){
+
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this); //initializes element described in FindBy
+    }
+
+
+
+}
