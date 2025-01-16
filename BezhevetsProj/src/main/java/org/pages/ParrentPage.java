0a96cc42705e74c18708a338b90fafb5a98c8792
Vlad <bezhevets.vv@gmail.com>
2025-01-16 21:54:39 +0200

    CW4 First Test with page object was created

---
+++ BezhevetsProj/src/main/java/org/pages/ParrentPage.java
@@ -0,0 +1,9 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParrentPage extends CommonActionsWithElements {
+    public ParrentPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
