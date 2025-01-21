c2e25435bd5e8b429a35085bc8c57d0f486c88b4
Mariasia13 <marjana.dm@gmail.com>
2025-01-21 21:56:29 +0200

    CommonActionsWithElements was updated

--- DmytryshynProj/src/main/java/org/pages/LoginPage.java
+++ DmytryshynProj/src/main/java/org/pages/LoginPage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends ParentPage{ //create constructor
+public class LoginPage extends ParentPage { //create constructor
     private Logger logger = Logger.getLogger(getClass());
 
-    @FindBy(xpath = "//input[@placeholder='Username']" )
+    @FindBy(xpath = "//input[@placeholder='Username']")
     private WebElement inputUserName;
 
-    @FindBy(xpath = "//input[@placeholder='Password']" )
+    @FindBy(xpath = "//input[@placeholder='Password']")
     private WebElement inputPassword;
 
-    @FindBy(xpath = "//button[text()='Sign In']" )
+    @FindBy(xpath = "//button[text()='Sign In']")
     private WebElement buttonSignIn;
 
     public LoginPage(WebDriver webDriver) {
@@ -29,20 +29,21 @@ public class LoginPage extends ParentPage{ //create constructor
 
     public void enterTextIntoInputLogin(String login) {
 //        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
-        inputUserName.clear();
-        inputUserName.sendKeys(login);
-        logger.info(login + " was inputted into input UserName");
+//        inputUserName.clear();
+//        inputUserName.sendKeys(login);
+//        logger.info(login + " was inputted into input UserName");
+        clearAndEnterTextIntoElement(inputUserName, login);
     }
 
     public void enterTextIntoInputPassword(String password) {
 //        WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
-        inputPassword.clear();
-        inputPassword.sendKeys(password);
-        logger.info(password + " password was inputted into input password");
+//        inputPassword.clear();
+//        inputPassword.sendKeys(password);
+//        logger.info(password + " password was inputted into input password");
+        clearAndEnterTextIntoElement(inputPassword, password);
     }
 
     public void clickOnButtonSignIn() {
-        buttonSignIn.click();
-        logger.info("Button SignIn was clicked");
+        clickOnElement(buttonSignIn);
     }
 }
