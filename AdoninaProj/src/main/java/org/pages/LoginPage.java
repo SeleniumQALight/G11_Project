d89be889c0a7337d30e06adea7862df6b6b9cc10
vadonina <vadonina@credible.com>
2025-01-21 21:56:40 +0200

    CW4:commonActionsWithElements was updated

--- AdoninaProj/src/main/java/org/pages/LoginPage.java
+++ AdoninaProj/src/main/java/org/pages/LoginPage.java
@@ -29,20 +29,21 @@ public class LoginPage extends ParentPage {
 
   public void enterTextIntoInputLogin(String login) {
 //    WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
-    inputUserName.clear();
-    inputUserName.sendKeys(login);
-    logger.info(login + " was inputted into input UserName");
+//    inputUserName.clear();
+//    inputUserName.sendKeys(login);
+//    logger.info(login + " was inputted into input UserName");
+    clearAndEnterTextIntoInput(inputUserName, login);
   }
 
   public void enterTextIntoInputPassw0rd(String password) {
 //    WebElement inputPassword = webDriver.findElement(By.xpath("//input[@placeholder='Password']"));
-    inputPassword.clear();
-    inputPassword.sendKeys(password);
-    logger.info(password + " was inputted into input password");
+//    inputPassword.clear();
+//    inputPassword.sendKeys(password);
+//    logger.info(password + " was inputted into input password");
+    clearAndEnterTextIntoInput(inputPassword, password);
   }
 
   public void clickOnButtonSignIn() {
-    buttonSignIn.click();
-    logger.info("Button SingIn was clicked");
+    clickOnElement(buttonSignIn);
   }
 }
