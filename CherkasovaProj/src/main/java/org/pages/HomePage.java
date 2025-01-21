68681598f3504aaecdb6978de88dd14f0490079f
tanyache <nsns@gmail.com>
2025-01-21 21:56:25 +0200

    commonActionWithElements was updated

--- CherkasovaProj/src/main/java/org/pages/HomePage.java
+++ CherkasovaProj/src/main/java/org/pages/HomePage.java
@@ -1,9 +1,9 @@
 package org.pages;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage{
     private Logger logger = Logger.getLogger(getClass());
@@ -11,19 +11,24 @@ public class HomePage extends ParentPage{
         super(webDriver);
     }
 
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSignOut;
+
 
     public void checkIsButtonSignOutVisible() {
-        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+//        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
+        checkIsElementVisible(buttonSignOut);
     }
 
-    private boolean isButtonSignOutVisible() {
-        try {
-            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-            logger.info(state + " is element visible");
-            return state;
-        } catch (Exception e) {
-            logger.info("Element is not found");
-            return false;
-        }
-    }
+//    private boolean isButtonSignOutVisible() {
+//        try {
+//            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+//            logger.info(state + " is element visible");
+//            return state;
+//        } catch (Exception e) {
+//            logger.info("Element is not found");
+//            return false;
+//        }
+//    }
+
 }
