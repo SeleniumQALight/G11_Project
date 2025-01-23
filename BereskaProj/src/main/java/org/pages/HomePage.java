85cac3c187a8ba95cc22dc88143ab21e59227cdf
obereska <75841845+obereska@users.noreply.github.com>
2025-01-23 22:10:02 +0200

    CW_6 created tests for PostPage

--- BereskaProj/src/main/java/org/pages/HomePage.java
+++ BereskaProj/src/main/java/org/pages/HomePage.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage {
-    private Logger logger = Logger.getLogger(getClass());
+  //  private Logger logger = Logger.getLogger(getClass());
 
     @FindBy(xpath = "//button[text()='Sign Out']")
     private WebElement buttonSignOut;
-    private Logger logger1 = Logger.getLogger(getClass());
+
+    @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
+    private WebElement buttonCreatePost;
 
 
     public HomePage(WebDriver webDriver) {
@@ -24,15 +26,14 @@ public class HomePage extends ParentPage {
         checkIsElementVisible(buttonSignOut);
     }
 
-    //      private boolean isButtonSignOutVisible() {
-    //      try {
-    //      boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-    //      logger.info(state + " is element visible");
-    //      return state;
-    //      } catch (Exception e) {
-    //      logger.info("Element is not found");
-    //      return false;
-    //      }
-    //      }
+    public HomePage checkIsRedirectOnHomePage() {
+        checkIsButtonSignOutVisible();
+        // TODO check current URL
+        return this;
+    }
+    public CreateNewPostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreateNewPostPage(webDriver);
+    }
 }
 
