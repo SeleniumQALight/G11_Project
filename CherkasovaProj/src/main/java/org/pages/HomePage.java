6c69341609434119b72282521ba2e05aede26729
tanyache <nsns@gmail.com>
2025-01-23 22:15:34 +0200

    CreateNewPostTest part_1

--- CherkasovaProj/src/main/java/org/pages/HomePage.java
+++ CherkasovaProj/src/main/java/org/pages/HomePage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage{
     private Logger logger = Logger.getLogger(getClass());
+
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -14,21 +16,24 @@ public class HomePage extends ParentPage{
     @FindBy(xpath = "//button[text()='Sign Out']")
     private WebElement buttonSignOut;
 
+    @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
+    private WebElement buttonCreatePost;
+
 
     public void checkIsButtonSignOutVisible() {
 //        Assert.assertTrue("Button Sign Out is not visible", isButtonSignOutVisible());
         checkIsElementVisible(buttonSignOut);
     }
 
-//    private boolean isButtonSignOutVisible() {
-//        try {
-//            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-//            logger.info(state + " is element visible");
-//            return state;
-//        } catch (Exception e) {
-//            logger.info("Element is not found");
-//            return false;
-//        }
-//    }
+    public HomePage checkIsRedirectToHomePage() {
+        checkIsButtonSignOutVisible();
+        //TODO check current URL
+        return this;
+    }
+
+    public CreateNewPostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreateNewPostPage(webDriver);
+    }
 
 }
--- CherkasovaProj/src/main/java/org/pages/LoginPage.java
+++ CherkasovaProj/src/main/java/org/pages/LoginPage.java
@@ -1,6 +1,7 @@
 package org.pages;
 
 import org.apache.log4j.Logger;
+import org.data.TestData;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -45,5 +46,13 @@ public class LoginPage extends ParentPage {
 
     public void clickOnButtonSignIn() {
         clickOnElement(buttonSignIn);
+    }
+
+    public HomePage openLoginPageAndFillLoginFormWithValidCred() {
+        openPage();
+        enterTextIntoInputLogin(TestData.VALID_LOGIN);
+        this.enterTextIntoInputPassword(TestData.VALID_PASSWORD);
+        clickOnButtonSignIn();
+        return new HomePage(webDriver);
     }
 }
