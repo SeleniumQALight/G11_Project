55d9e4c77d1ce9ac337f20590c6ab059967af37c
Yurii Sobko <yurasobko@gmail.com>
2025-01-23 19:02:43 +0200

    HW 2  invalid login

--- SobkoProj/src/main/java/org/pages/HomePage.java
+++ SobkoProj/src/main/java/org/pages/HomePage.java
@@ -1,6 +1,8 @@
 package org.pages;
 
+
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -12,7 +14,17 @@ public class HomePage extends ParentPage {
     }
 
     public void checkIsButtonSignOutVisible() {
+        Assert.assertTrue("Button Sign Out is not visible", isButtonSighOutVisible());
+    }
+    public void checkIsButtonSignOutNotVisible() {
+        Assert.assertFalse("Button Sign Out is not visible", isButtonSighOutVisible());
+    }
 
+    public void checkIsButtonSignInVisible() {
+        Assert.assertTrue("Button Sign Out is not visible", isButtonSighInVisible());
+    }
+    public void checkIsWarningMessageVisible() {
+        Assert.assertTrue("Button Sign Out is not visible", isWarningMessageVisible());
     }
 
     private boolean isButtonSighOutVisible() {
@@ -24,4 +36,25 @@ public class HomePage extends ParentPage {
             return false;
         }
     }
+
+    private boolean isButtonSighInVisible() {
+        try {
+            boolean state = webdriver.findElement(By.xpath("//button[text()='Sign In']")).isDisplayed();
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not found");
+            return false;
+        }
+    }
+    private boolean isWarningMessageVisible() {
+        try {
+            boolean state = webdriver.findElement(By.xpath("//div[text()='Invalid username/password.']")).isDisplayed();
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not found");
+            return false;
+        }
+    }
+
+
 }
--- SobkoProj/src/test/java/baseTest/LoginTestWithPageObject.java
+++ SobkoProj/src/test/java/baseTest/LoginTestWithPageObject.java
@@ -11,4 +11,15 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.geLoginPage().clickOnButtonSignIn();
         pageProvider.getHomePage().checkIsButtonSignOutVisible();
     }
+    @Test
+    public void T0001_inValidLogin() {
+        pageProvider.geLoginPage().openPage();
+        pageProvider.geLoginPage().enterTextIntoInputLogin("qaauto");
+        pageProvider.geLoginPage().enterTextIntoInputPassword("123456qwerty1");
+        pageProvider.geLoginPage().clickOnButtonSignIn();
+        pageProvider.getHomePage().checkIsButtonSignOutNotVisible();
+        pageProvider.getHomePage().checkIsButtonSignInVisible();
+        pageProvider.getHomePage().checkIsWarningMessageVisible();
+
+    }
 }
