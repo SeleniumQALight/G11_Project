115819a76552961dd302e78795154e44970ed169
tanyache <nsns@gmail.com>
2025-01-23 19:57:24 +0200

    TestData was added and method chain

--- CherkasovaProj/src/main/java/org/pages/LoginPage.java
+++ CherkasovaProj/src/main/java/org/pages/LoginPage.java
@@ -21,25 +21,26 @@ public class LoginPage extends ParentPage {
         super(webDriver);
     }
 
-    public void openPage() {
+    public LoginPage openPage() {
         String baseUrl = "https://aqa-complexapp.onrender.com";
         webDriver.get(baseUrl);
         logger.info("Login Page was opened with url " + baseUrl);
+        return this;
 
     }
 
-    public void enterTextIntoInputLogin(String login) {
+    public LoginPage enterTextIntoInputLogin(String login) {
 //        WebElement inputUserName = webDriver.findElement(By.xpath("//input[@placeholder='Username']"));
 //        inputUserName.clear();
 //        inputUserName.sendKeys(login);
 //        logger.info(login + " was inputted into input UserName");
         clearAndEnterTextIntoElement(inputUserName, login);
-
+        return this;
     }
 
-    public void enterTextIntoInputPassword(String password) {
+    public LoginPage enterTextIntoInputPassword(String password) {
         clearAndEnterTextIntoElement(inputPassword, password);
-
+        return this;
     }
 
     public void clickOnButtonSignIn() {
