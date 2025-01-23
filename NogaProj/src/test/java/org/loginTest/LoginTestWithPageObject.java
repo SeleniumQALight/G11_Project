87748aa1f35fb42147a0341fc71b0ae444400177
Anton Noga <noga.anton@pdffiller.team>
2025-01-23 19:57:22 +0200

    test data was added and update with chain method

--- NogaProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
+++ NogaProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
@@ -3,13 +3,17 @@ package org.loginTest;
 import org.baseTest.BaseTest;
 import org.junit.Test;
 
+import static org.data.TestData.VALID_LOGIN;
+import static org.data.TestData.VALID_PASSWORD;
+
 public class LoginTestWithPageObject extends BaseTest {
     @Test
     public void T0001_validLogin() {
-        pageProvider.getLoginPage().openPage();
-        pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto");
-        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
-        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.getLoginPage()
+                .openPage()
+                .enterTextIntoInputLogin(VALID_LOGIN)
+                .enterTextIntoInputPassword(VALID_PASSWORD)
+                .clickOnButtonSignIn();
 
         pageProvider.getHomePage().checkIsButtonSignOutVisible();
     }
