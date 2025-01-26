f611668757d3a4a63f308244992d16c73c0923d0
Vlad <bezhevets.vv@gmail.com>
2025-01-26 18:16:11 +0200

    CreateNewPostTest part_1

--- BezhevetsProj/src/main/java/org/pages/HomePage.java
+++ BezhevetsProj/src/main/java/org/pages/HomePage.java
@@ -10,11 +10,25 @@ public class HomePage extends ParrentPage {
     @FindBy(xpath = "//button[text()='Sign Out']")
     private WebElement buttonSignOut;
 
+    @FindBy(xpath = "//a[@class='btn btn-sm btn-success mr-2']")
+    private WebElement buttonCreatePost;
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void checkIsButtonSignOutVisible() {
         checkIsElementVisible(buttonSignOut);
+    }
+
+    public HomePage checkIsRedirectToHomePage() {
+        checkIsButtonSignOutVisible();
+        //TODO check current URL
+        return this;
+    }
+
+    public CreateNewPostPage clickOnButtonCreatePost() {
+        clickOnElement(buttonCreatePost);
+        return new CreateNewPostPage(webDriver);
     }
 }
