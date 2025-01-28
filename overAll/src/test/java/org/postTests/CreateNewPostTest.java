f0ab56bdf57db347fd6da845985b2c2c960fc6a7
obereska <75841845+obereska@users.noreply.github.com>
2025-01-28 21:58:37 +0200

    CW_7 - Added after for deleting posts

--- overAll/src/test/java/org/postTests/CreateNewPostTest.java
+++ overAll/src/test/java/org/postTests/CreateNewPostTest.java
@@ -1,9 +1,14 @@
 package org.postTests;
 
 import org.baseTest.BaseTest;
+import org.junit.After;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    // GUID = 1b1b1b1b-1b1b-1b1b-1b1b-1b1b1b1b1b1b
+
+    final String POST_TITLE = "TR003_bereska_" + Utils_Custom.getDateAndTimeFormatted();
     @Test
     public void TR003_createNewPost() {
         pageProvider.getLoginPage()
@@ -11,7 +16,7 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToHomePage()
                 .getHeaderElement().clickOnButtonCreatePost()
                 .checkIsRedirectToCreateNewPostPage()
-                .enterTextIntoInputTitle("Title Taras")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("Body")
                 .clickOnSaveNewPostButton()
                 .checkIsRedirectToPostPage()
@@ -20,7 +25,14 @@ public class CreateNewPostTest extends BaseTest {
 
         ;
 
-        pageProvider.getPostPage().getHeaderForUserElement().clickOnButtonMyProfile();
+        pageProvider.getPostPage().getHeaderForUserElement().clickOnButtonMyProfile()
+                .checkIsRedirectToProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE, 1);
+    }
+
+    @After
+    public void deletePosts() {
+
     }
 
 }
