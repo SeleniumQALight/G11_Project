099bbd0e2141b7468a28bd84fb1b8361bb350158
NRyabokon <nryabokon30@gmail.com>
2025-01-28 17:28:12 +0100

    some additional corrections

--- RyabokonProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
+++ RyabokonProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
@@ -2,7 +2,6 @@ package org.loginTest;
 
 
 import org.baseTest.BaseTest;
-import org.data.TestData;
 import org.junit.Test;
 
 import static org.data.TestData.VALID_LOGIN;
@@ -23,13 +22,13 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getHomePage().checkIsButtonSignOutVisible();
     }
 
-/*@Test our homework
-    public void T002_invalidLogin() {
+@Test
+    public void T002a_invalidLogin() {
     pageProvider.getLoginPage().openPage();
     pageProvider.getLoginPage().enterTextIntoInputLogin("qaato1");
     pageProvider.getLoginPage().enterTextIntoInputPassword("123456");
     pageProvider.getLoginPage().clickOnButtonSignIn();
     pageProvider.getHomePage().checkIsButtonSignOutVisible();
-}*/
+}
 }
 
