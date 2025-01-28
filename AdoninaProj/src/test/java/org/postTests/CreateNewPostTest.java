cd95a4af1911a21db2decd515015768c1403c313
vadonina <vadonina@credible.com>
2025-01-28 21:42:48 +0200

    CW5: Check of number post was added

--- AdoninaProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ AdoninaProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -2,8 +2,13 @@ package org.postTests;
 
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+  // GUID = 1b3b3b3b-0b3b-4b3b-8b3b-2b3b3b3b3b3b
+
+  final String POST_TITLE = "TR003_adonVA_" + Utils_Custom.getDateAndTimeFormatted();
+
   @Test
   public void TR003_CreateNewPost() {
     pageProvider.getLoginPage()
@@ -11,7 +16,7 @@ public class CreateNewPostTest extends BaseTest {
             .checkIsRedirectOnHomePage()
             .getHeaderElement().clickOnButtonCreatePost()
             .checkIsRedirectToCreateNewPostPage()
-            .enterTextIntoInputTitle("Title of the post VA")
+            .enterTextIntoInputTitle(POST_TITLE)
             .enterTextIntoInputBody("Body of the post")
             .clickOnButtonSaveNewPost()
             .checkIsRedirectToPostPage()
@@ -20,6 +25,8 @@ public class CreateNewPostTest extends BaseTest {
     ;
 
     pageProvider.getPostPage().getHeaderElement().clickOnButtonMyProfile()
+            .checkIsRedirectToMyProfilePage()
+            .checkPostWithTitleIsPresent(POST_TITLE, 1)
     ;
 
   }
