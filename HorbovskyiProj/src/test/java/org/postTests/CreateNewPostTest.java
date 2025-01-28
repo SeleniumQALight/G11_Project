2f551545627968099724a3fa45bcd7758e312217
O-Horbovskyi <a.horbovskyi@gmail.com>
2025-01-28 21:42:48 +0200

    Check for number of posts was added

--- HorbovskyiProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ HorbovskyiProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -2,8 +2,10 @@ package org.postTests;
 
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    final String POST_TITLE = "TR003Horbovskyi" + Utils_Custom.getDateAndTimeFormatted();
 
     @Test
     public void TR003_createNewPost() {
@@ -20,6 +22,8 @@ public class CreateNewPostTest extends BaseTest {
                 .checkTextInSuccessMessage("New post successfully created.")
         ;
         pageProvider.getPostPage().getHeaderElement().clickOnButtonMyProfile()
+                .checkIsRedirectToMyProfilePage()
+                .checkIsPostWithTitleWereAdded(POST_TITLE, 1)
         ;
 
     }
