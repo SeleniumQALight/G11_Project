97c516136cc4f2f13943c8f5ac7bf537c64a50bf
nikolay.savchenko <nikolay.savchenko@alldigitalads.com>
2025-01-28 21:42:42 +0200

    check for number of posts was added

--- SavchenkoProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ SavchenkoProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -2,23 +2,31 @@ package org.postTests;
 
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    //GUID =
     @Test
     public void TR003_createNewPost() {
+
+        final String POST_TITLE = "TR003_savchenko_" + Utils_Custom.getDateAndTimeFormatted();
+
         pageProvider.getLoginPage()
                 .openLoginPageAndFillLoginFormWithValidCred()
                 .checkIsRedirectToHomePage()
                 .getHeaderElement().clickOnButtonCreatePost()
                 .checkIsRedirectToCreateNewPostPage()
-                .enterTextIntoInputTitle("Title Nickolay")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("Body")
                 .clickOnSaveNewPostButton()
                 .checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
                 .checkTextInSuccessMessage("New post successfully created.")
         ;
-        pageProvider.getPostPage().getHeaderElement().clickOnButtonMyProfile();
+        pageProvider.getPostPage().getHeaderElement().clickOnButtonMyProfile()
+                .checkIsRedirectToProfilePage()
+                .checkPostWithTitlePresent(POST_TITLE, 1)
+        ;
 
 
     }
