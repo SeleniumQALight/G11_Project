e544df9d07c32091a219e2e529d986dba0102c63
MaksymTreshchov <gamesonl94@gmail.com>
2025-01-28 21:43:00 +0200

    Check for number of posts

--- TreshchovProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ TreshchovProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -2,8 +2,12 @@ package org.postTests;
 
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    //GUID =
+
+    final String TITLE = "Treshchov Test" + Utils_Custom.getDateAndTimeFormatted();
     @Test
     public void TR003_createNewPost() {
         pageProvider.getLoginPage().
@@ -12,7 +16,7 @@ public class CreateNewPostTest extends BaseTest {
                 getHeaderElement().
                 clickOnButtonCreatePost().
                 checkIsRedirectToCreateNewPostPage().
-                enterTextIntoInputTitle("Test123Treshchov").
+                enterTextIntoInputTitle(TITLE).
                 enterTextIntoInputBody("test body").
                 clickOnButtonSavePost().
                 checkIsRedirectToPostPage().
@@ -22,7 +26,7 @@ public class CreateNewPostTest extends BaseTest {
 
         pageProvider.getPostPage().
                 getHeaderElement().
-                clickOnButtonMyProfile()
+                clickOnButtonMyProfile().checkIsRedirectToMyProfilePage().checkPostWithTitlePresent(TITLE, 1)
 
         ;
     }
