a4ae62dc61cd9baad5d075eb8fc9caea816e0956
Vlad <bezhevets.vv@gmail.com>
2025-01-28 21:51:14 +0200

    deletePosts method creating is started

--- BezhevetsProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ BezhevetsProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -1,6 +1,7 @@
 package org.postTests;
 
 import org.baseTest.BaseTest;
+import org.junit.After;
 import org.junit.Test;
 import org.utils.Utils_Custom;
 
@@ -26,6 +27,11 @@ public class CreateNewPostTest extends BaseTest {
         ;
 
         pageProvider.getPostPage().getHeaderElement().clickOnMyProfileButton().checkIsRedirectToMyProfilePage().checkPostWithTitleIsPresent(POST_TITLE, 1);
+    }
+
+    @After
+    public void deletePosts() {
+
     }
 
 }
