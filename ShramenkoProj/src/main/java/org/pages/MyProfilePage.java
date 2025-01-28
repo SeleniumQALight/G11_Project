776595a954d834b9ea7231a87cb450ed1d424413
ellie-star <ashramenko@gmail.com>
2025-01-28 20:41:40 +0100

    Check for numbers of post was added into CreateNewPostTest

--- ShramenkoProj/src/main/java/org/pages/MyProfilePage.java
+++ ShramenkoProj/src/main/java/org/pages/MyProfilePage.java
@@ -1,10 +1,37 @@
 package org.pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class MyProfilePage extends ParentPage {
 
+    private String postWithTitleLocator = "//*[text()='%s']"; //параметризованний локатор
+
     public MyProfilePage(WebDriver webDriver) {
         super(webDriver);
+    }
+
+    //findElements знайде всі елементи за таким локатором і поверне List (повний або пустий):
+
+    private List<WebElement> getPostsList(String postTitle) {
+        return webDriver.findElements(
+                By.xpath(
+                        String.format(postWithTitleLocator, postTitle)
+                ));
+    }
+
+    public MyProfilePage checkIsRedirectToMyProfilePage() {
+        //TODO checkUrl();
+        return this;
+    }
+
+    public MyProfilePage checkPostWithTitleIsPresent(String postTitle, int numberOfPosts) {
+        Assert.assertEquals("Number of posts with title " + postTitle,
+                numberOfPosts, getPostsList(postTitle).size());
+        return this;
     }
 }
--- ShramenkoProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ ShramenkoProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -2,8 +2,15 @@ package org.postTests;
 
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.utils.Utils_Custom;
 
 public class CreateNewPostTest extends BaseTest {
+    //GUID = 1b1b1b1b-1b1b-1b1b-1b1b-1b1b1b1b1b1b
+    //GUID гарантує унікальність, але не несе змісту
+
+
+    //унікальність забезпечуємо за допомогою дати і часу
+    final String POST_TITLE = "TR003_Shramenko_" + Utils_Custom.getDateAndTimeFormatted();
 
     @Test
     public void TR003_createNewPost() {
@@ -13,7 +20,7 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToHomePage()
                 .getHeaderElement().clickOnButtonCreatePost()
                 .checkIsRedirectToCreateNewPostPage()
-                .enterTextIntoInputTitle("Title of the post from Alla")
+                .enterTextIntoInputTitle(POST_TITLE)
                 .enterTextIntoInputBody("Body of the post")
                 .clickOnButtonSaveNewPost().checkIsRedirectToPostPage()
                 .checkIsSuccessMessageDisplayed()
@@ -21,6 +28,8 @@ public class CreateNewPostTest extends BaseTest {
         ;
 
         pageProvider.getPostPage().getHeaderElement().clickOnMyProfileButton()
+                .checkIsRedirectToMyProfilePage()
+                .checkPostWithTitleIsPresent(POST_TITLE, 1) //працює поки наш пост тільки один
         ;
     }
 
