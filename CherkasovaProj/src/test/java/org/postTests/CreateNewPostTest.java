53ba61309acd6564738dd6a35e1c702c0e9252e9
tanyache <nsns@gmail.com>
2025-01-29 19:20:03 +0200

    After

--- CherkasovaProj/src/test/java/org/postTests/CreateNewPostTest.java
+++ CherkasovaProj/src/test/java/org/postTests/CreateNewPostTest.java
@@ -1,6 +1,7 @@
 package org.postTests;
 
 import org.baseTest.BaseTest;
+import org.junit.After;
 import org.junit.Test;
 import org.utils.Utils_Custom;
 
@@ -28,5 +29,10 @@ public class CreateNewPostTest extends BaseTest {
                 .checkIsRedirectToProfilePage()
                 .checkIsPostWithTitleIsPresent(POST_TITLE, 1)
         ;
+    }
+
+    @After
+    public void deletePosts() {
+
     }
 }
