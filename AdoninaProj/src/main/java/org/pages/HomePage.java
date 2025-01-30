d41d525d0232f690621e7959b13f096eda512db1
vadonina <vadonina@credible.com>
2025-01-30 21:13:41 +0200

    CreateNewPostTest After was added

--- AdoninaProj/src/main/java/org/pages/HomePage.java
+++ AdoninaProj/src/main/java/org/pages/HomePage.java
@@ -1,11 +1,13 @@
 package org.pages;
 
 
+import org.apache.log4j.Logger;
+import org.data.TestData;
 import org.openqa.selenium.WebDriver;
 import org.pages.elements.HeaderForUserElement;
 
 public class HomePage extends ParentPage {
-//  private Logger logger = Logger.getLogger(getClass());
+  private Logger logger = Logger.getLogger(getClass());
 
 
   public HomePage(WebDriver webDriver) {
@@ -19,6 +21,21 @@ public class HomePage extends ParentPage {
   public HomePage checkIsRedirectOnHomePage() {
     getHeaderElement().checkIsButtonSignOutVisible();
     //TODO check current URL
+    return this;
+  }
+
+  public HomePage openHomePageAndLoginIfNeeded() {
+    LoginPage loginPage = new LoginPage(webDriver);
+    loginPage.openPage();
+    if (getHeaderElement().isButtonSignOutVisible()) {
+      logger.info("User is already logged in");
+    } else {
+      loginPage.enterTextIntoInputLogin(TestData.VALID_LOGIN);
+      loginPage.enterTextIntoInputPassw0rd(TestData.VALID_PASSWORD);
+      loginPage.clickOnButtonSignIn();
+      checkIsRedirectOnHomePage();
+      logger.info("User was logged in");
+    }
     return this;
   }
 
