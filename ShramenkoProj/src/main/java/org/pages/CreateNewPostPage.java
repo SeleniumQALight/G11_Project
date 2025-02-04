2920004d818099746e604c953784d23cbbef6fa9
ellie-star <ashramenko@gmail.com>
2025-02-04 20:52:58 +0100

    checkUrl and checkUrlWithPattern were created. CreateNewPostTest is green.

--- ShramenkoProj/src/main/java/org/pages/CreateNewPostPage.java
+++ ShramenkoProj/src/main/java/org/pages/CreateNewPostPage.java
@@ -22,8 +22,13 @@ public class CreateNewPostPage extends ParentPage {
         super(webDriver);
     }
 
+    @Override
+    protected String getRelativeUrl() {
+        return "/create-post";
+    }
+
     public CreateNewPostPage checkIsRedirectToCreateNewPostPage() {
-        //TODO check current url
+        checkUrl();
         return this;
     }
 
--- ShramenkoProj/src/main/java/org/pages/PostPage.java
+++ ShramenkoProj/src/main/java/org/pages/PostPage.java
@@ -19,13 +19,18 @@ public class PostPage extends ParentPage {
         super(webDriver);
     }
 
+    @Override
+    protected String getRelativeUrl() {
+        return "/post/[a-zA-Z0-9]*"; //регулярний вираз [a-zA-Z0-9]* - будь-яка послідовність і кількість цифр і букв
+    }
+
     //отримати доступ до всіх елементів з хедеру
     public HeaderForUserElement getHeaderElement() {
         return new HeaderForUserElement(webDriver);
     }
 
     public PostPage checkIsRedirectToPostPage() {
-        //TODO check current url
+        checkUrlWithPattern();
         return this;
     }
 
