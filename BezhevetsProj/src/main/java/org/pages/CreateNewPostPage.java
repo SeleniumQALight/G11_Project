9419078c4dd137c6b50be52ced31e7c467a87e61
Vlad <bezhevets.vv@gmail.com>
2025-02-04 21:16:47 +0200

    work with Dropdown

--- BezhevetsProj/src/main/java/org/pages/CreateNewPostPage.java
+++ BezhevetsProj/src/main/java/org/pages/CreateNewPostPage.java
@@ -14,8 +14,16 @@ public class CreateNewPostPage extends ParrentPage {
     @FindBy(xpath = "//button[text()='Save New Post']")
     private WebElement buttonSaveNewPost;
 
+    @FindBy(xpath = "//select")
+    private WebElement dropdownAccess;
+
     public CreateNewPostPage(WebDriver webDriver) {
         super(webDriver);
+    }
+
+    public CreateNewPostPage selectValueInDropDownAccess(String valueInDropDown) {
+        selectValueInDD(dropdownAccess, valueInDropDown);
+        return this;
     }
 
     public CreateNewPostPage checkIsRedirectToCreateNewPostPage() {
--- BezhevetsProj/src/main/java/org/pages/PostPage.java
+++ BezhevetsProj/src/main/java/org/pages/PostPage.java
@@ -14,6 +14,8 @@ public class PostPage extends ParrentPage {
     @FindBy(xpath = "//button[@class='delete-post-button text-danger']")
     private WebElement buttonDeletePost;
 
+    private String locatorForTextThisPostWasWritten = "//*[contains(text(),'%s')]";
+
     public PostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -40,5 +42,10 @@ public class PostPage extends ParrentPage {
     public MyProfilePage clickOnDeleteButton() {
         clickOnElement(buttonDeletePost, "Delete post button");
         return new MyProfilePage(webDriver);
+    }
+
+    public PostPage checkTextThisPostWasWrittenIsVisible(String text) {
+        checkIsElementVisible(String.format(locatorForTextThisPostWasWritten, text));
+        return this;
     }
 }
