b6a38332660bf9520e81a2899230bde0729c4353
tanyache <nsns@gmail.com>
2025-02-04 21:17:09 +0200

    work with DropDown

--- CherkasovaProj/src/main/java/org/pages/CreateNewPostPage.java
+++ CherkasovaProj/src/main/java/org/pages/CreateNewPostPage.java
@@ -18,6 +18,9 @@ public class CreateNewPostPage extends ParentPage {
     @FindBy(xpath = ".//input[@name='uniquePost']")
     private WebElement checkbox;
 
+    @FindBy(xpath = ".//select")
+    private WebElement dropdownAccess;
+
     public CreateNewPostPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -25,6 +28,11 @@ public class CreateNewPostPage extends ParentPage {
 
     public CreateNewPostPage checkIsRedirectToCreateNewPostPage() {
 //       TODO checkUrl();
+        return this;
+    }
+
+    public CreateNewPostPage selectValueInDropDownAccess(String valueForSelect) {
+        selectValueInDD(dropdownAccess, valueForSelect);
         return this;
     }
 
--- CherkasovaProj/src/main/java/org/pages/PostPage.java
+++ CherkasovaProj/src/main/java/org/pages/PostPage.java
@@ -15,6 +15,8 @@ public class PostPage extends ParentPage {
     @FindBy(xpath = ".//button[@class='delete-post-button text-danger']")
     private WebElement buttonDeletePost;
 
+    private String locatorForTextThisPostWasWritten = "//*[contains(text(),'%s')]";
+
 
     public PostPage(WebDriver webDriver) {
         super(webDriver);
@@ -48,5 +50,10 @@ public class PostPage extends ParentPage {
     public MyProfilePage clickOnDeleteButton() {
         clickOnElement(buttonDeletePost, "Delete post button");
         return new MyProfilePage(webDriver);
+    }
+
+    public PostPage checkTextThisPostWasWrittenIsVisible(String text) {
+        checkIsElementVisible(String.format(locatorForTextThisPostWasWritten, text));
+        return this;
     }
 }
