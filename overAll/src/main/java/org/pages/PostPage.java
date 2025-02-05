41bc8bdcb2da9c9e1b19a1c549ee3d98ec20e3a4
ellie-star <ashramenko@gmail.com>
2025-02-05 21:17:39 +0100

    postpage

--- overAll/src/main/java/org/pages/PostPage.java
+++ overAll/src/main/java/org/pages/PostPage.java
@@ -4,20 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.pages.elements.HeaderForUserElement;
-import org.apache.log4j.Logger;
 
 public class PostPage extends ParentPage {
     // "//*[contains(@class,'alert-success')]"
     @FindBy(xpath = "//*[@class='alert alert-success text-center']")
     private WebElement successMessage;
-
-    @FindBy(xpath = "//p[text()='Is this post unique? : yes']")
-    private WebElement checkboxUniquePostYes;
-
-    @FindBy(xpath = "//p[text()='Is this post unique? : no']")
-    private WebElement checkboxUniquePostNo;
-
-    Logger logger = Logger.getLogger(getClass());
 
     public PostPage(WebDriver webDriver) {
         super(webDriver);
@@ -41,16 +32,4 @@ public class PostPage extends ParentPage {
         checkTextInElement(successMessage, expectedMessageText);
         return this;
     }
-
-    public void checkStateUniquePost(){
-        try {
-            if (checkboxUniquePostYes.isDisplayed()) {
-                logger.info("It's unique post");
-            }
-        } catch (Exception e) {
-            logger.info("It's not unique post");
-        }
-    }
-
-
 }
