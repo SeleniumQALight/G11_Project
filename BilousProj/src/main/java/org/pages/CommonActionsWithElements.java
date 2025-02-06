b60f1d58a0c7ff802760c3cf45eee8bd63481e51
dronbilous <andrii1981bilous@gmail.com>
2025-02-06 21:02:41 +0100

    ValidationMessageTest

--- BilousProj/src/main/java/org/pages/CommonActionsWithElements.java
+++ BilousProj/src/main/java/org/pages/CommonActionsWithElements.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -175,7 +176,39 @@ public class CommonActionsWithElements {
         }
     }
 
+    // acceptAlert
+    protected void acceptAlert() {
+        try {
+            webDriverWait_10.until(ExpectedConditions.alertIsPresent());
+            webDriver.switchTo().alert().accept();
+            logger.info("Alert was accepted");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
 
+    // scroll to element using Actions
+    protected void scrollToElement(WebElement webElement) {
+        try {
+            Actions actions = new Actions(webDriver);
+            actions.moveToElement(webElement);
+            actions.perform();
+            logger.info("Scrolled to element " + getElementName(webElement));
+
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
+
+    // open new tab using JS
+    protected void openNewTab() {
+        try {
+            ((org.openqa.selenium.JavascriptExecutor) webDriver).executeScript("window.open()");
+            logger.info("New tab was opened");
+        } catch (Exception e) {
+            printErrorAndStopTest(e);
+        }
+    }
 
     private void printErrorAndStopTest(Exception e) {
         logger.error("Can not work with element " + e);
