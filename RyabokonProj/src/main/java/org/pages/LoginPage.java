b6786ac0f8ed7019fa5e0f37e6a44855c289d127
NRyabokon <nryabokon30@gmail.com>
2025-02-11 19:25:22 +0100

    CW 11/02

--- RyabokonProj/src/main/java/org/pages/LoginPage.java
+++ RyabokonProj/src/main/java/org/pages/LoginPage.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.utils.Utils_Custom;
 
 import java.util.List;
 
@@ -104,6 +105,7 @@ return this;
         String[] messagesArray = expectedErrors.split(RegistrationValidationMessages.SEMICOLON);
 
         webDriverWait10.until((ExpectedConditions.numberOfElementsToBe(By.xpath(listErrorMessagesLocator), messagesArray.length)));
+        Utils_Custom.waitABit(1);
 
         Assert.assertEquals("Number of Messages",messagesArray.length, listOfActualMessages.size());
         SoftAssertions softAssertions = new SoftAssertions();
