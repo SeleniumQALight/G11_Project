51bf77d7c6c5b40f34c8ab4ee16c10b0332b84ce
tradulenko <radulenko@gmail.com>
2025-02-11 20:25:02 +0200

    params

--- overAll/src/test/java/org/registrationTest/ValidationMessageTest.java
+++ overAll/src/test/java/org/registrationTest/ValidationMessageTest.java
@@ -1,25 +1,34 @@
 package org.registrationTest;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.baseTest.BaseTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.data.RegistrationValidationMessages.*;
 
+@RunWith(JUnitParamsRunner.class)
 public class ValidationMessageTest extends BaseTest {
     @Test
-    public void TC023_validationMessagesTest() {
+    @Parameters(method = "parametersForValidationMessagesTest")
+    public void TC023_validationMessagesTest(
+            String userName, String email, String password, String expectedMessages) {
         pageProvider.getLoginPage().openPage();
         pageProvider.getLoginPage()
-                .enterTextIntoRegistrationUserNameField("tr")
-                .enterTextIntoRegistrationEmailField("tr")
-                .enterTextIntoRegistrationPasswordField("tr")
-                .checkErrorsMessages(ERROR_USERNAME
-                                    + SEMICOLON
-                                    + ERROR_EMAIL
-                                    + SEMICOLON
-                                    + ERROR_PASSWORD
-                );
-        ;
+                .enterTextIntoRegistrationUserNameField(userName)
+                .enterTextIntoRegistrationEmailField(email)
+                .enterTextIntoRegistrationPasswordField(password)
+                .checkErrorsMessages(expectedMessages);
+
+    }
+
+    public Object[][] parametersForValidationMessagesTest(){
+        return new Object[][]{
+                {"tr", "tr", "tr", ERROR_USERNAME + SEMICOLON + ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD},
+                {"ttrr", "tr", "tr", ERROR_EMAIL + SEMICOLON + ERROR_PASSWORD}
+
+        };
     }
 
 }
