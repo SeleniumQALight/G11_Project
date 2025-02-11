5686dbd6259b6a81275626c43147bb9a8604ac79
ellie-star <ashramenko@gmail.com>
2025-02-11 19:55:02 +0100

    envs

--- ShramenkoProj/src/main/java/org/pages/ParentPage.java
+++ ShramenkoProj/src/main/java/org/pages/ParentPage.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 
 abstract public class ParentPage extends CommonActionsWithElements {
-    protected String baseUrl = "https://aqa-complexapp.onrender.com";
+
+    String environment = System.getProperty("env", "aqa");
+    protected String baseUrl = "https://" + environment + "-complexapp.onrender.com";
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
@@ -13,15 +15,15 @@ abstract public class ParentPage extends CommonActionsWithElements {
     abstract protected String getRelativeUrl();
 
     protected void checkUrl() {
-            Assert.assertEquals("URL is not expected"
-                    , baseUrl + getRelativeUrl()
-                    , webDriver.getCurrentUrl());
+        Assert.assertEquals("URL is not expected"
+                , baseUrl + getRelativeUrl()
+                , webDriver.getCurrentUrl());
     }
     //перевірка чи поточний URL відповідає шаблону
     //https://aqa-complexapp.onrender.com/post/5f7b3b3b8f3b9d0017f3b3b3
     //regex for 5f7b3b3b8f3b9d0017f3b3b3 is [a-zA-Z0-9]{24} - 24 символи букви або цифри, можна не вказувати
 
-    protected void checkUrlWithPattern(){
+    protected void checkUrlWithPattern() {
         Assert.assertTrue("URL is not expected \n" +
                         "Expected url: " + baseUrl + getRelativeUrl() +
                         "\n Actual url: " + webDriver.getCurrentUrl(),
