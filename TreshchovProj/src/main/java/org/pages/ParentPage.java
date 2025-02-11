2ac616d5efb46b741736473154e7f38aab838330
MaksymTreshchov <gamesonl94@gmail.com>
2025-02-11 21:55:50 +0200

    CW 11 config options

--- TreshchovProj/src/main/java/org/pages/ParentPage.java
+++ TreshchovProj/src/main/java/org/pages/ParentPage.java
@@ -2,10 +2,12 @@ package org.pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.utils.ConfigProvider;
 
 abstract public class ParentPage extends CommonActionsWithElements {
     String environment = System.getProperty("env", "aqa");
-    protected String baseUrl = "https://"+environment+"-complexapp.onrender.com";
+    // protected String baseUrl = "https://"+environment+"-complexapp.onrender.com";
+    protected String baseUrl = ConfigProvider.configProperties.base_url().replace("[env]", environment);
 
     public ParentPage(WebDriver webDriver) {
         super(webDriver);
