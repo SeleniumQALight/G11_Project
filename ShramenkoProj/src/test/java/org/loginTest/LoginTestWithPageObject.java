e981bae942b34a12070e14318afa8c8180925f4e
ellie-star <ashramenko@gmail.com>
2025-02-20 18:51:59 +0100

    Suites

--- ShramenkoProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
+++ ShramenkoProj/src/test/java/org/loginTest/LoginTestWithPageObject.java
@@ -1,18 +1,21 @@
 package org.loginTest;
 
 import org.baseTest.BaseTest;
+import org.categories.SmokeTestsFilter;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Category(SmokeTestsFilter.class)
     public void T0001_validLogin() {
         pageProvider.getLoginPage()
                 .openLoginPageAndFillLoginWithValidCred();
 
         //а тепер обов'язкова перевірка, чи все вийшло
         pageProvider.getHomePage().getHeaderElement().checkIsButtonSighOutVisible();
-        pageProvider.getLoginPage().checkIsButtonSighInNotVisible();
-        pageProvider.getLoginPage().checkIsInputLoginAndPasswordNotVisible();
+//        pageProvider.getLoginPage().checkIsButtonSighInNotVisible();
+//        pageProvider.getLoginPage().checkIsInputLoginAndPasswordNotVisible();
 
     }
 
