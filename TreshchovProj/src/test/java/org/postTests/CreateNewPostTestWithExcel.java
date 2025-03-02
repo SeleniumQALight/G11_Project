66e86a561c496387697a05bbb9ac7dbdd27d0fb4
MaksymTreshchov <gamesonl94@gmail.com>
2025-03-02 13:17:12 +0200

    fixed merge conflicts and fixed @after to delete posts

--- TreshchovProj/src/test/java/org/postTests/CreateNewPostTestWithExcel.java
+++ TreshchovProj/src/test/java/org/postTests/CreateNewPostTestWithExcel.java
@@ -19,19 +19,19 @@ import java.util.Collection;
 public class CreateNewPostTestWithExcel extends BaseTest {
     Logger logger = Logger.getLogger(CreateNewPostTestWithExcel.class);
 
-    final String time = Utils_Custom.getDateAndTimeFormatted();
+    String titleUnique;
 
     @Test
     @Parameters(method = "parametersForCreatePostTest")
     public void HW6_createNewPostUsingExcel(String title, String body, String dropdownSelect, String checkBox, String expectedMessage, String expectedUniquePost) {
-        String titleUnique = String.format(title, "Treshchov Test", Utils_Custom.getDateAndTimeFormatted());
+        titleUnique = String.format(title, "Treshchov Test", Utils_Custom.getDateAndTimeFormatted());
         pageProvider.getLoginPage().
                 openLoginAndFillLoginFormWithValidData().
                 checkIsRedirectToHomePage().
                 getHeaderElement().
                 clickOnButtonCreatePost().
                 checkIsRedirectToCreateNewPostPage().
-                enterTextIntoInputTitle(String.format(title, "Treshchov Test", time)).
+                enterTextIntoInputTitle(titleUnique).
                 selectValueInDD(dropdownSelect).
                 enterTextIntoInputBody(String.format(body,"Treshchov Test")).
                 checkBoxSelection(checkBox).
@@ -45,14 +45,14 @@ public class CreateNewPostTestWithExcel extends BaseTest {
 
         pageProvider.getPostPage().
                 getHeaderElement().
-                clickOnButtonMyProfile().checkIsRedirectToMyProfilePage().checkPostWithTitlePresent((String.format(title, "Treshchov Test", time)), 1)
+                clickOnButtonMyProfile().checkIsRedirectToMyProfilePage().checkPostWithTitlePresent(titleUnique, 1)
 
         ;
     }
 
     @After
     public void deletePosts(){
-        pageProvider.getHomePage().openHomePageAndLoginIfNeeded().getHeaderElement().clickOnButtonMyProfile().checkIsRedirectToMyProfilePage().deletePostsTillPresent(String.format("%s-%s-excel", "Treshchov Test", time));
+        pageProvider.getHomePage().openHomePageAndLoginIfNeeded().getHeaderElement().clickOnButtonMyProfile().checkIsRedirectToMyProfilePage().deletePostsTillPresent(titleUnique);
 
     }
 
