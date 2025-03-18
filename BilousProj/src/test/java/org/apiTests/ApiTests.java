2e222be420b2f5c23d01749ed3a95f39ac1ba74a
dronbilous <andrii1981bilous@gmail.com>
2025-03-18 19:21:29 +0100

    api cw-3 lombok, builder

--- BilousProj/src/test/java/org/apiTests/ApiTests.java
+++ BilousProj/src/test/java/org/apiTests/ApiTests.java
@@ -46,10 +46,28 @@ public class ApiTests {
         }
 
         PostsDTO[] expectedResponse = {
-                new PostsDTO("The second Default post", "This post was created automatically after cleaning the database",
-                        "All Users", "no", new AuthorDTO(USER_NAME), false),
-                new PostsDTO("The first Default post", "This post was created automatically after cleaning the database",
-                        "All Users", "no", new AuthorDTO(USER_NAME), false)
+                PostsDTO.builder()
+                        .title("The second Default post")
+                        .body("This post was created automatically after cleaning the database")
+                        .select("All Users")
+                        .uniquePost("no")
+                        .author(AuthorDTO.builder().username(USER_NAME).build())
+                        .isVisitorOwner(false)
+                        .build(),
+                PostsDTO.builder()
+                        .title("The first Default post")
+                        .body("This post was created automatically after cleaning the database")
+                        .select("All Users")
+                        .uniquePost("no")
+                        .author(AuthorDTO.builder().username(USER_NAME).build())
+                        .isVisitorOwner(false)
+                        .build()
+
+
+//                new PostsDTO("The second Default post", "This post was created automatically after cleaning the database",
+//                        "All Users", "no", new AuthorDTO(USER_NAME), false),
+//                new PostsDTO("The first Default post", "This post was created automatically after cleaning the database",
+//                        "All Users", "no", new AuthorDTO(USER_NAME), false)
 
         };
 
