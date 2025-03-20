948263a6d9b17eaac84bbd4f684a7cd92f5d2b1c
dronbilous <andrii1981bilous@gmail.com>
2025-03-20 21:01:21 +0100

    cw4

--- BilousProj/src/main/java/org/data/TestData.java
+++ BilousProj/src/main/java/org/data/TestData.java
@@ -2,8 +2,14 @@ package org.data;
 
 import org.utils.ConfigProvider;
 
+import java.util.Locale;
+
 public class TestData {
     public static final String VALID_LOGIN = System.getProperty
             ("defaultLogin", ConfigProvider.configHiddenProperties.login());
     public static final String VALID_PASSWORD = "123456qwerty";
+
+    public static final String VALID_LOGIN_API = "bilous55555".toLowerCase(Locale.ROOT);
+    public static final String VALID_PASSWORD_API = "bil123456789".toLowerCase(Locale.ROOT);
+
 }
