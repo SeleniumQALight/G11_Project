d652d272c94bddfdcce3b090c29b4a122640b695
Vasylyna Khasanova <wassilissa.ws@gmail.com>
2025-03-25 13:26:13 +0200

    AddBookToUserProfileByApiTest was added

---
+++ KhasanovaProj/src/main/java/org/api/BookStoreEndPoints.java
@@ -0,0 +1,10 @@
+package org.api;
+
+public interface BookStoreEndPoints {
+    String BASE_URL = "https://demoqa.com";
+    String LOGIN = BASE_URL + "/Account/v1/Login";
+    String ALL_BOOKS = BASE_URL + "/BookStore/v1/Books";
+    String GET_ALL_BOOKS_OF_USER = BASE_URL + "/Account/v1/User/{0}";
+
+
+}
