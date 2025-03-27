745f58567df804f883e08ae2bcaddc49dbda3798
Mariasia13 <marjana.dm@gmail.com>
2025-03-27 21:45:03 +0200

    e2e bdd + ui + api

--- DmytryshynProj/src/main/java/org/bdd/stepDefinitions/HomePageMyStepdefs.java
+++ DmytryshynProj/src/main/java/org/bdd/stepDefinitions/HomePageMyStepdefs.java
@@ -1,7 +1,11 @@
 package org.bdd.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
+
+import static org.bdd.stepDefinitions.MainSteps.DEFAULT;
 
 public class HomePageMyStepdefs extends MainSteps {
     public HomePageMyStepdefs(WebDriverHelper webDriverHelper) {
@@ -11,5 +15,22 @@ public class HomePageMyStepdefs extends MainSteps {
     @Then("I see avatar on HomePage")
     public void iSeeAvatarOnHomePage() {
         pageProvider.getHomePage().getHeaderElement().checkIsButtonSingOutVisible();
+    }
+
+    @And("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String userName, String password) {
+        if (DEFAULT.equalsIgnoreCase(userName)) {
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_PASSWORD_API;
+        }
+        pageProvider.getLoginPage()
+                .openPage()
+                .enterTextIntoInputLogin(userName)
+                .enterTextIntoInputPassword(password)
+                .clickOnButtonSignIn();
+
+        pageProvider.getHomePage().checkIsRedirectToHomePage();
     }
 }
