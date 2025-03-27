0040027378171024ea32c6f678037a7708f438cd
Yurii Sobko <yurasobko@gmail.com>
2025-03-27 19:43:27 +0200

    Add BDD case with params 27.03

--- SobkoProj/src/main/java/org/bdd/stepDefinitions/LoginPageStepsDefinition.java
+++ SobkoProj/src/main/java/org/bdd/stepDefinitions/LoginPageStepsDefinition.java
@@ -15,6 +15,7 @@ public class LoginPageStepsDefinition extends MainSteps {
     public void iOpenLoginPage() {
         pageProvider.getLoginPage().openPage();
     }
+
     @When("I login with valid cred")
     public void iLoginWithValidCred() {
         pageProvider.getLoginPage().enterTextIntoInputLogin(TestData.VALID_LOGIN)
@@ -24,5 +25,25 @@ public class LoginPageStepsDefinition extends MainSteps {
 
     }
 
+    @When("I enter {string} into input Login in Login page")
+    public void i_enter_into_input_login_in_login_page(String userName) {
+        pageProvider.getLoginPage().enterTextIntoInputLogin(userName);
+    }
+
+    @When("I enter {string} into input PassWord in Login page")
+    public void i_enter_into_input_pass_word_in_login_page(String password) {
+pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+    }
+
+    @When("I click on button SignIn in Login page")
+    public void i_click_on_button_sign_in_in_login_page() {
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+
+    @Then("I see alert message with text {string}")
+    public void i_see_alert_message_with_text(String errorMessage) {
+        pageProvider.getLoginPage().checkTextInAlertMessage(errorMessage);
+
+    }
 
 }
--- SobkoProj/src/test/resources/features/login.feature
+++ SobkoProj/src/test/resources/features/login.feature
@@ -6,3 +6,18 @@
       Given I open Login page
       When I login with valid cred
       Then I see avatar on HomePage
+
+
+    Scenario Outline: R002Login with invalid creads
+      Given I open Login page
+      When I enter '<login>' into input Login in Login page
+      And I enter '<password>' into input PassWord in Login page
+      And I click on button SignIn in Login page
+      Then I see alert message with text 'Invalid username/password.'
+
+
+      Examples:
+        | login           | password           |
+        | qaauto          | not_valid_password |
+        | not_valid_login | 123456qwerty       |
+
