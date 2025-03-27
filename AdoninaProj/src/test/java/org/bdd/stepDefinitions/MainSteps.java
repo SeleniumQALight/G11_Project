e4f6ec02d8322f918254eaa6eb60f788add448e4
vadonina <vadonina@credible.com>
2025-03-27 21:45:00 +0200

    CW6: e2e BDD+API+UI

--- AdoninaProj/src/test/java/org/bdd/stepDefinitions/MainSteps.java
+++ AdoninaProj/src/test/java/org/bdd/stepDefinitions/MainSteps.java
@@ -7,6 +7,8 @@ public class MainSteps {
   protected WebDriverHelper webDriverHelper;
   protected PageProvider pageProvider;
 
+  public static final String DEFAULT = "default";
+
   public MainSteps(WebDriverHelper webDriverHelper){
     this.webDriverHelper = webDriverHelper;
     pageProvider = new PageProvider(webDriverHelper.getWebDriver());
