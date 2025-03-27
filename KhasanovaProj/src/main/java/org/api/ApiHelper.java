93eecdfddea83f28e88383c4d58db060de58f27f
Vasylyna Khasanova <wassilissa.ws@gmail.com>
2025-03-27 21:45:12 +0200

    e2e bdd + ui + api

--- KhasanovaProj/src/main/java/org/api/ApiHelper.java
+++ KhasanovaProj/src/main/java/org/api/ApiHelper.java
@@ -10,11 +10,13 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
+import org.api.dto.requestDTO.CreatePostDTO;
 import org.api.dto.responseDTO.PostsDTO;
 import org.data.TestData;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -94,5 +96,25 @@ public class ApiHelper {
                 .delete(EndPoints.DELETE_POST, id)
                 .then()
                 .spec(responseSpecification);
+    }
+
+    public void createPosts(Integer numberOfPosts, String token, Map<String, String> postsData) {
+        for (int i = 0; i < numberOfPosts; i++) {
+            CreatePostDTO bodyOfPostCreation =
+                    CreatePostDTO.builder()
+                            .title(postsData.get("title") + " " + i) //значення з дататейбл
+                            .body(postsData.get("body"))
+                            .select1(postsData.get("select"))
+                            .uniquePost("no")
+                            .token(token)
+                            .build();
+            given()
+                    .spec(requestSpecification)
+                    .body(bodyOfPostCreation)
+                    .when()
+                    .post(EndPoints.CREATE_POST)
+                    .then()
+                    .spec(responseSpecification);
+        }
     }
 }
--- KhasanovaProj/src/main/java/org/bdd/stepDefinitions/Hook.java
+++ KhasanovaProj/src/main/java/org/bdd/stepDefinitions/Hook.java
@@ -2,10 +2,13 @@ package org.bdd.stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.api.ApiHelper;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
 
 public class Hook {
     WebDriverHelper webDriverHelper;
+    ApiHelper apiHelper = new ApiHelper();
 
     public Hook(WebDriverHelper webDriverHelper) {
         this.webDriverHelper = webDriverHelper; //за допомогою пікоконтейнеру ми можемо передати об'єкт класу WebDriverHelper
@@ -13,13 +16,21 @@ public class Hook {
         //якщо є, то створюється, якщо немає, то створюється все що потрібно для створення об'єкта
     }
 
-    @Before
+    @Before(order = 10) //цифра вказує порядок виконання хуків, запускаються від найменшого до найбільшого
     public void setUp() {
 //        webDriverHelper = new WebDriverHelper();
     }
 
-    @After
+    @After(order = 15) //цифра вказує порядок виконання хуків, запускаються від найбільшого до найменшого
     public void tearDown() {
         webDriverHelper.quiteDriver();
     }
+
+    @Before(value = "@deletePostsTillPresentForDefaultUser", order = 50)
+    @After(value = "@deletePostsTillPresentForDefaultUser", order = 50)
+    public void deletePostsTillPresent(){
+        apiHelper.deleteAllPostsTillPresent(TestData.VALID_LOGIN_API,
+                apiHelper.getToken());
+    }
 }
+
