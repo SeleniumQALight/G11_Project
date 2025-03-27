d1ccecab1e34ade7e69aa3903d7e85414d3d741f
Mariasia13 <marjana.dm@gmail.com>
2025-03-27 19:43:15 +0200

    bdd case with params

--- DmytryshynProj/src/main/java/org/bdd/stepDefinitions/LoginPageStepsDefinitions.java
+++ DmytryshynProj/src/main/java/org/bdd/stepDefinitions/LoginPageStepsDefinitions.java
@@ -22,4 +22,27 @@ public class LoginPageStepsDefinitions extends MainSteps {
         pageProvider.getLoginPage().enterTextIntoInputPassword(TestData.VALID_PASSWORD);
         pageProvider.getLoginPage().clickOnButtonSignIn();
     }
+
+    @When("I enter {string} into input Login in Login page")
+    public void i_enter_into_input_login_in_login_page(String userName) {
+        pageProvider.getLoginPage().enterTextIntoInputLogin(userName);
+    }
+
+    @When("I enter {string} into input PassWord in Login page")
+    public void i_enter_into_input_pass_word_in_login_page(String password) {
+        pageProvider.getLoginPage().enterTextIntoInputPassword(password);
+    }
+
+    @When("I click on button SignIn in Login page")
+    public void i_click_on_button_sign_in_in_login_page() {
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+    }
+
+    @Then("I see alert message with text {string}")
+    public void i_see_alert_message_with_text(String errorMessage) {
+        pageProvider.getLoginPage().checkTextInAlertInCenter(errorMessage);
+    }
+
+
+
 }
