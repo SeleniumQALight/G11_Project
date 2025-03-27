a269b9ccb52b5c36925781f624218cf8b76be6ee
dronbilous <andrii1981bilous@gmail.com>
2025-03-27 20:44:00 +0100

    case with params bbd

--- BilousProj/src/main/java/org/bdd/stepDefinitions/HomepageStepdefs.java
+++ BilousProj/src/main/java/org/bdd/stepDefinitions/HomepageStepdefs.java
@@ -1,7 +1,10 @@
 package org.bdd.stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
 
 public class HomepageStepdefs extends MainSteps {
     public HomepageStepdefs(WebDriverHelper webDriverHelper) {
@@ -12,4 +15,24 @@ public class HomepageStepdefs extends MainSteps {
     public void iSeeAvatarOnHomePage() {
         pageProvider.getHomePage().getHeaderElement().checkIsButtonSignOutVisible();
     }
+
+    @And("I open Home page as {string} user and {string} password")
+    public void iOpenHomePageAsDefaultUserAndDefaultPassword(String userName, String password) {
+        if (MainSteps.DEFAULT.equalsIgnoreCase(userName)) {
+            userName = TestData.VALID_LOGIN_API;
+        }
+        if (MainSteps.DEFAULT.equalsIgnoreCase(password)) {
+            password = TestData.VALID_PASSWORD_API;
+        }
+        pageProvider.getLoginPage()
+                .openPage()
+                .enterTextIntoInputLogin(userName)
+                .enterTextIntoPassword(password)
+                .clickInButtonSignIn();
+        
+        pageProvider.getHomePage().checkIsRedirectOnHomePage();
+        
+    }
+
+
 }
