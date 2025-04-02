14489d0d1e8749e3beb3141e014e2ae8419c451f
NRyabokon <nryabokon30@gmail.com>
2025-04-02 17:59:09 +0200

    HW Books

---
+++ RyabokonProj/src/main/java/org/api/BookEndPoints.java
@@ -0,0 +1,14 @@
+package org.api;
+
+public interface BookEndPoints {
+
+
+   String BASE_URL = "https://demoqa.com";
+    String LOGIN = BASE_URL + "/Account/v1/Login";
+   String DELETE_BOOKS = BASE_URL +  "/BookStore/v1/Book";
+   String GET_BOOKS = "https://demoqa.com/BookStore/v1/Books";
+   String ADD_BOOK = "https://demoqa.com/BookStore/v1/Books";
+   String GET_USER = "https://demoqa.com/Account/v1/User/";
+
+
+}
