c71e2634fe87218ea02c15c0ec798d4e79743bb6
Vlad <bezhevets.vv@gmail.com>
2025-04-09 20:30:01 +0300

    EXAM_PROJECT

--- BezhevetsProj/src/main/java/org/api/PrivatBankEndpoints.java
+++ BezhevetsProj/src/main/java/org/api/PrivatBankEndpoints.java
@@ -3,4 +3,5 @@ package org.api;
 public interface PrivatBankEndpoints {
     String BASE_PB_URL = "https://api.privatbank.ua";
     String EXCHANGE_RATES = BASE_PB_URL + "/p24api/exchange_rates";
+    String CURRENCY_RATES = BASE_PB_URL + "/p24api/pubinfo";
 }
