332e21a7c2197e9ff0c16aca1e1c29b305b3865a
NRyabokon <nryabokon30@gmail.com>
2025-04-15 23:47:19 +0200

    Exam UI and API, part 2nd was added

--- RyabokonProj/src/main/java/org/api/PrivateBankEndPoints.java
+++ RyabokonProj/src/main/java/org/api/PrivateBankEndPoints.java
@@ -5,7 +5,7 @@ public interface PrivateBankEndPoints {
 
         String BASE_URLBank = "https://api.privatbank.ua";
         String CURRENCY_EXCHANGE = BASE_URLBank + "/p24api/exchange_rates";
-        String CURRENCY_RATES_API = BASE_URLBank + "/p24api/pubinfo?json&exchange&coursid=5";
+        String CURRENCY_RATES_API = BASE_URLBank + "/p24api/pubinfo";
 
     }
 
