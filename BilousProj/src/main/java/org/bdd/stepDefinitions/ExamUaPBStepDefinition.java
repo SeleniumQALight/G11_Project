92ef509d2f9acfa2106ed09216e90cd4e7ab6a8a
dronbilous <andrii1981bilous@gmail.com>
2025-04-17 02:45:37 +0200

    exam add Click on button

--- BilousProj/src/main/java/org/bdd/stepDefinitions/ExamUaPBStepDefinition.java
+++ BilousProj/src/main/java/org/bdd/stepDefinitions/ExamUaPBStepDefinition.java
@@ -17,6 +17,7 @@ public class ExamUaPBStepDefinition extends MainSteps{
 
     @Then("I fetch the {string} exchange rate from PrivatBank UI")
     public void iFetchTheExchangeRateFromPrivatBankUI(String arg0, String arg1) {
+        pageProvider.getExamPbMainPage().clickExchangeRatesButton();
 
     }
 }
